ff3cb0d963470790c1076daef239bf15bbf1d8b7
Noel <odonnell.noel@gmail.com>
2018-11-30 14:47:09 +0000

    Basic redux/saga boilerplate in place

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
+import {createStore, applyMiddleware, compose} from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import createBrowserHistory from 'history/createBrowserHistory'
+import {routerMiddleware} from 'react-router-redux'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-const rootDiv = document.getElementById('root')
+import rootReducer from './reducers'
+import rootSaga from './sagas'
 
-ReactDOM.render(<App/>, rootDiv)
+const rootDiv = document.getElementById('root')
+const history = createBrowserHistory()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const sagaMiddleware = createSagaMiddleware()
+
+
+const store = createStore(
+    rootReducer,
+    {}, // preloaded state
+    composeEnhancers(
+        applyMiddleware(sagaMiddleware),
+        applyMiddleware(routerMiddleware(history))
+    )
+)
+
+sagaMiddleware.run(rootSaga)
+
+ReactDOM.render(<App store={store}/>, rootDiv)
---
+++ src/sagas/index.js
@@ -0,0 +1,5 @@
+function* rootSaga() {
+    console.log('root saga running')
+}
+
+export default rootSaga
